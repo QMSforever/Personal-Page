5386ebc9bc5418a6804c868abe09f7610596db24
“QMSforever” <D582649699@HOTMAIL.COM>
2026-06-06 20:53:19 -0700

    Use relative asset base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Personal-Page/' : '/',
+  base: mode === 'production' ? './' : '/',
   plugins: [react()],
 }))
